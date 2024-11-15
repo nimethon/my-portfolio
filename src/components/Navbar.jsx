50fa9827ffe8fa4d36764b28dd4e8ec6c1efdb8e
Md. Nazimul Islam Methon <86713755+nimethon@users.noreply.github.com>
2024-11-15 22:22:21 +0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,11 +13,11 @@ const Navbar = () => {
                 <a href=""><img className="mx-1 w-4/12" src={logo} alt="Methon" /></a>
             </div>
             <div className="m-8 flex item-center justify-center gap-4 text-2xl">
-                <a href="https://linkedin.com/in/methon"><FaLinkedin /></a>
-                <a href="https://github.com/nimethon"><FaGithub /></a>
-                <a href="https://x.com/Methon_"><FaTwitterSquare /></a>
-                <a href="https://www.facebook.com/nimethon/"><FaFacebookSquare /></a>
-                <a href=""><FaInstagram /></a>
+                <a href="https://linkedin.com/in/methon" target="_blank"><FaLinkedin /></a>
+                <a href="https://github.com/nimethon" target="_blank"><FaGithub /></a>
+                <a href="https://x.com/Methon_" target="_blank"><FaTwitterSquare /></a>
+                <a href="https://www.facebook.com/nimethon/" target="_blank"><FaFacebookSquare /></a>
+                <a href="https://www.instagram.com/ni._.methon/" target="_blank"><FaInstagram /></a>
             </div>
         </nav>
 
